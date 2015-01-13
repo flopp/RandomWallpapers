264f40d17fc785033d91a844e557ecaa946fe092
flopp <fpigorsch@gmail.com>
2015-01-13 17:55:42 +0100

    added copyright

--- tools/gnome-set-background.sh
+++ tools/gnome-set-background.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Set wallpaper in Gnome, Unity, etc.
+
+# Copyright (C) 2015 Florian Pigorsch <mail@florian-pigorsch.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if [ $# -ne 1 -o "$1" == "--help" ] ; then
   echo "USAGE: $0 IMAGE_FILE"
   exit 1
